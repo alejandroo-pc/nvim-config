8947c040ba22fa0734b4db1c29ae2a065e3fdb8b
Alejandro Patino Camargo <alejandro@Alejandros-MacBook-Pro.local>
2023-07-12 23:39:03 -0700

    added lsp plugins, better terminal mappings and modified buffer aesthetic**

--- init.lua
+++ init.lua
@@ -1,3 +1,23 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.cmd.colorscheme("tokyonight")
+
+require("toggleterm").setup({
+	open_mapping = [[<leader>t]], -- Set the open mapping to <leader>t
+	start_in_insert = true,
+	insert_mappings = true,
+	persist_size = false,
+	direction = "horizontal",
+})
+
+-- setting bufferline options
+vim.opt.termguicolors = true
+require("bufferline").setup({
+	options = {
+		separator_style = "slant",
+		always_show_bufferline = true,
+		show_buffer_close_icons = true,
+		show_close_icon = true,
+		color_icons = true,
+	},
+})
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,19 @@
+return {
+	"neovim/nvim-lspconfig",
+	opts = {
+		servers = {
+			tailwindcss = {
+				filetypes_exclude = { "markdown" },
+			},
+		},
+		setup = {
+			tailwindcss = function(_, opts)
+				local tw = require("lspconfig.server_configurations.tailwindcss")
+				--- @param ft string
+				opts.filetypes = vim.tbl_filter(function(ft)
+					return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+				end, tw.default_config.filetypes)
+			end,
+		},
+	},
+}
