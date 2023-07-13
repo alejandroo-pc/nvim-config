b4c7cf4100f0b61ddafabf524b0b807e8c1ee581
alejandro-patino-camargo <alejandropatinoc@gmail.com>
2023-07-12 19:16:04 -0700

    initial commit

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,14 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  opts = {
+    suggestion = { enabled = false },
+    panel = { enabled = false },
+    filetypes = {
+      javascript = true,
+      markdown = true,
+      help = true,
+    },
+  },
+}
