1f8c1e2e93f101f57f5f1d3b3cdf08a36962609a
Alejandro Patino Camargo <alejandro@Alejandros-MacBook-Pro.local>
2023-08-18 11:13:46 -0700

    removed unused dependencies
    added yanky for better copy/paste

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,14 +1,14 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  build = ":Copilot auth",
-  opts = {
-    suggestion = { enabled = false },
-    panel = { enabled = false },
-    filetypes = {
-      javascript = true,
-      markdown = true,
-      help = true,
-    },
-  },
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	build = ":Copilot auth",
+	opts = {
+		suggestion = { enabled = true },
+		panel = { enabled = false },
+		filetypes = {
+			javascript = true,
+			markdown = true,
+			help = true,
+		},
+	},
 }
