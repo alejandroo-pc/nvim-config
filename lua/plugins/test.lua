df3045c1bbd62b172bffd05ddf68e2f956aa1f6a
alejandro-pc <alejandropatinoc@gmail.com>
2024-04-06 14:36:25 -0700

    updated testing commands

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -5,6 +5,7 @@ return {
 			"thenbe/neotest-playwright",
 			"haydenmeade/neotest-jest",
 		},
+		lazy = false,
 		config = function()
 			require("neotest").setup({
 				consumers = {
@@ -12,54 +13,20 @@ return {
 				},
 				adapters = {
 					require("neotest-playwright").adapter({
-						playwrightCommand = "npx playwright test",
-						env = { CI = true },
-						cwd = function()
-							return vim.fn.getcwd()
-						end,
 						options = {
 							persist_project_selection = true,
 							enable_dynamic_test_discovery = true,
 						},
-					}),
-					require("neotest-jest")({
-						jestCommand = "npm test --",
-						jestConfigFile = "custom.jest.config.ts",
 						env = { CI = true },
 						cwd = function()
 							return vim.fn.getcwd()
 						end,
 					}),
 				},
-				keys = {
-					{
-						"<leader>tp",
-						function()
-							require("neotest").playwright.attachment()
-						end,
-						desc = "Launch test attachment",
-					},
-					{
-						"<leader>tl",
-						function()
-							require("neotest").run.run_last()
-						end,
-						desc = "Run Last Test",
-					},
-					{
-						"<leader>tL",
-						function()
-							require("neotest").run.run_last({ strategy = "dap" })
-						end,
-						desc = "Debug Last Test",
-					},
-					{
-						"<leader>tw",
-						"<cmd>lua require('neotest').run.run({ jestCommand = 'jest --watch ' })<cr>",
-						desc = "Run Watch",
-					},
-				},
 			})
+			local keymap = vim.keymap
+			keymap.set("n", "<leader>ta", "<cmd>NeotestPlaywrightProject<CR>", { desc = "Attatch Playwright" })
+			keymap.set("n", "<leader>tf", "<cmd>NeotestPlaywrightRefresh<CR>", { desc = "Playwright Refresh" })
 		end,
 	},
 }
