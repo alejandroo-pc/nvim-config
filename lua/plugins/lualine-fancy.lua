6e9b0acb15160f8758300afe44c087c0363361da
alejandro-pc <alejandropatinoc@gmail.com>
2024-05-02 00:03:11 -0700

    added better lualine

---
+++ lua/plugins/lualine-fancy.lua
@@ -0,0 +1,45 @@
+return {
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+			"meuter/lualine-so-fancy.nvim",
+		},
+		opts = {
+			options = {
+				theme = "onedark",
+				component_separators = { left = "│", right = "│" },
+				section_separators = { left = "", right = "" },
+				globalstatus = true,
+				refresh = {
+					statusline = 100,
+				},
+			},
+			sections = {
+				lualine_a = {
+					{ "fancy_mode", width = 3 },
+				},
+				lualine_b = {
+					{ "fancy_branch" },
+				},
+				lualine_c = {
+					{
+						-- "fancy_cwd",
+						-- substitute_home = true,
+					},
+				},
+				lualine_x = {
+					{ "fancy_diff" },
+					{ "fancy_macro" },
+					{ "fancy_diagnostics" },
+					{ "fancy_searchcount" },
+					{ "fancy_location" },
+				},
+				lualine_y = {
+					{ "fancy_filetype", ts_icon = "" },
+				},
+				lualine_z = {},
+			},
+		},
+	},
+}
--- lua/plugins/navic.lua
+++ lua/plugins/navic.lua
@@ -1,4 +1,51 @@
 return {
 	"SmiteshP/nvim-navic",
 	requires = "neovim/nvim-lspconfig",
+	config = function()
+		local navic = require("nvim-navic")
+		navic.setup({
+			icons = {
+				File = "󰈙 ",
+				Module = " ",
+				Namespace = "󰌗 ",
+				Package = " ",
+				Class = "󰌗 ",
+				Method = "󰆧 ",
+				Property = " ",
+				Field = " ",
+				Constructor = " ",
+				Enum = "󰕘",
+				Interface = "󰕘",
+				Function = "󰊕 ",
+				Variable = "󰆧 ",
+				Constant = "󰏿 ",
+				String = "󰀬 ",
+				Number = "󰎠 ",
+				Boolean = "◩ ",
+				Array = "󰅪 ",
+				Object = "󰅩 ",
+				Key = "󰌋 ",
+				Null = "󰟢 ",
+				EnumMember = " ",
+				Struct = "󰌗 ",
+				Event = " ",
+				Operator = "󰆕 ",
+				TypeParameter = "󰊄 ",
+			},
+			lsp = {
+				auto_attach = true,
+				preference = nil,
+			},
+			highlight = true,
+			separator = " > ",
+			depth_limit = 5,
+			depth_limit_indicator = "..",
+			safe_output = true,
+			lazy_update_context = false,
+			click = false,
+			format_text = function(text)
+				return text
+			end,
+		})
+	end,
 }
