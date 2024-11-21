f8fb2b0a1bc0a04557be33b80047a98cc474bc39
Alejandro Patino Camargo <alejandro@Alejandros-MacBook-Pro.local>
2024-11-20 19:08:03 -0800

    updated dependencies

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,7 +19,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.linting.eslint" },
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.dap.core" },
+		--{ import = "lazyvim.plugins.extras.dap.core" },
 		--{ import = "lazyvim.plugins.extras.test.core" },
 		--{ import = "lazyvim.plugins.extras.coding.copilot" },
 		-- import/override with your plugins
