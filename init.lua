c10fecab2b016fbadc171ab57ceac8e81327bbbc
alejandro-pc <alejandropatinoc@gmail.com>
2025-01-14 11:42:27 -0800

    removed transparency

--- init.lua
+++ init.lua
@@ -23,23 +23,5 @@ require("neo-tree").setup({
 	},
 })
 
-require("transparent").setup({
-	-- table: additional groups that should be cleared
-	extra_groups = {
-		"NormalFloat",
-		"NvimTreeNormal", -- NvimTree
-	},
-	-- table: groups you don't want to clear
-	exclude_groups = {},
-	on_clear = function() end,
-})
-
-require("transparent").clear_prefix("NeoTree")
-require("transparent").clear_prefix("BufferLine")
-require("transparent").clear_prefix("lualine")
-require("transparent").clear_prefix("Whichkey")
-require("transparent").clear_prefix("Telescope")
-require("transparent").clear_prefix("mason")
-require("transparent").clear_prefix("snacks")
 
 vim.cmd.colorscheme("tokyonight-moon")
