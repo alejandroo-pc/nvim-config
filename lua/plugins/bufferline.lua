6f1c063c17fa2a26203cf07604c1313f2b213574
alejandro-pc <alejandropatinoc@gmail.com>
2025-02-01 08:01:53 -0800

    added buffer re-order to budder

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,6 +1,13 @@
 return {
 	"akinsho/bufferline.nvim",
 	version = "*",
-	lazy = true,
-	dependencies = "nvim-tree/nvim-web-devicons",
+	lazy = false,
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+
+	config = function()
+		local keymap = vim.api.nvim_set_keymap
+
+		keymap("n", "==", "<cmd>BufferLineMoveNext<CR>", { noremap = true, silent = true, desc = "Move buffer right" })
+		keymap("n", "--", "<cmd>BufferLineMovePrev<CR>", { noremap = true, silent = true, desc = "Move buffer left" })
+	end,
 }
