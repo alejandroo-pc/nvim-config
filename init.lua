4b4e00fa9b16781fc429abdb78e2fef6c42d03da
alejandro-pc <alejandropatinoc@gmail.com>
2025-02-02 10:50:45 -0800

    added dap config and adapter to nvim

--- init.lua
+++ init.lua
@@ -23,4 +23,47 @@ require("neo-tree").setup({
 	},
 })
 
+local dap = require("dap")
+
+dap.adapters["pwa-node"] = {
+	type = "server",
+	host = "localhost",
+	port = "${port}",
+	executable = {
+		command = "node",
+		args = { "/Users/alejandro/workspace/cellar/js-debug/src/dapDebugServer.js", "${port}" },
+	},
+}
+
+dap.configurations.javascript = {
+	{
+		name = "Next.js: debug server-side",
+		type = "pwa-node", -- Use pwa-node for Node.js debugging
+		request = "launch",
+		runtimeExecutable = "npm",
+		runtimeArgs = { "run", "dev" },
+		cwd = vim.fn.getcwd(), -- Current working directory
+	},
+	{
+		name = "Next.js: debug client-side",
+		type = "chrome",
+		request = "launch",
+		url = "http://localhost:3000",
+		webRoot = "${workspaceFolder}",
+	},
+	{
+		name = "Next.js: debug full stack",
+		type = "pwa-node", -- Use pwa-node for Node.js debugging
+		request = "launch",
+		runtimeExecutable = "npm",
+		runtimeArgs = { "run", "dev" },
+		cwd = vim.fn.getcwd(),
+		serverReadyAction = {
+			pattern = "started server on .+, url: (https?://.+)",
+			uriFormat = "%s",
+			action = "debugWithChrome",
+		},
+	},
+}
+
 vim.cmd.colorscheme("tokyonight-moon")
