161ea8415f3cd8f77e9daaba9fdb54436813dcb9
alejandro-pc <alejandropatinoc@gmail.com>
2025-02-21 19:30:31 -0800

    moved bufferline setup to file

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,8 +2,17 @@ return {
 	"akinsho/bufferline.nvim",
 	version = "*",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-
 	config = function()
+		require("bufferline").setup({
+			options = {
+				-- separator_style = "slant",
+				always_show_bufferline = true,
+				show_buffer_close_icons = true,
+				show_close_icon = true,
+				color_icons = true,
+			},
+		})
+
 		local keymap = vim.api.nvim_set_keymap
 
 		keymap("n", "==", "<cmd>BufferLineMoveNext<CR>", { noremap = true, silent = true, desc = "Move buffer right" })
