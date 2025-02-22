2158f7240b12cfd7b2e8a8df135b4680698f337a
alejandro-pc <alejandropatinoc@gmail.com>
2025-02-21 19:30:07 -0800

    removed setup function

--- init.lua
+++ init.lua
@@ -1,15 +1,5 @@
 require("config.lazy")
 
-require("bufferline").setup({
-	options = {
-		--separator_style = "slant",
-		always_show_bufferline = true,
-		show_buffer_close_icons = true,
-		show_close_icon = true,
-		color_icons = true,
-	},
-})
-
 require("neo-tree").setup({
 	window = {
 		position = "right",
