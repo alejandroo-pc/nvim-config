25d28e7e97b848dbeb3857dd6a60247b9a58069a
alejandro-pc <alejandropatinoc@gmail.com>
2025-02-22 11:03:23 -0800

    separated octo keys from plenary

--- lua/plugins/octo.lua
+++ lua/plugins/octo.lua
@@ -8,13 +8,18 @@ return {
 		default_merge_method = "squash",
 		picker = "telescope",
 	},
+
 	keys = {
-		{ "<leader>gi", "<cmd>Octo issue list<CR>", desc = "List Issues (Octo)" },
-		{ "<leader>gI", "<cmd>Octo issue search<CR>", desc = "Search Issues (Octo)" },
-		{ "<leader>gp", "<cmd>Octo pr list<CR>", desc = "List PRs (Octo)" },
-		{ "<leader>gP", "<cmd>Octo pr search<CR>", desc = "Search PRs (Octo)" },
-		{ "<leader>gr", "<cmd>Octo repo list<CR>", desc = "List Repos (Octo)" },
-		{ "<leader>gS", "<cmd>Octo search<CR>", desc = "Search (Octo)" },
+		{ "<leader>oi", "<cmd>Octo issue list<CR>", desc = "List Issues (Octo)" },
+		{ "<leader>oI", "<cmd>Octo issue search<CR>", desc = "Search Issues (Octo)" },
+		{ "<leader>op", "<cmd>Octo pr list<CR>", desc = "List PRs (Octo)" },
+		{ "<leader>oP", "<cmd>Octo pr search<CR>", desc = "Search PRs (Octo)" },
+		{ "<leader>or", "<cmd>Octo repo list<CR>", desc = "List Repos (Octo)" },
+		{ "<leader>oS", "<cmd>Octo search<CR>", desc = "Search (Octo)" },
+
+		{ "<leader>oR", "<cmd>Octo review start<CR>", desc = "Start PR Review (Octo)" },
+		{ "<leader>oC", "<cmd>Octo pr checkout<CR>", desc = "Checkout to PR branch (Octo)" },
+		{ "<leader>oL", "<cmd>Octo pr reload<CR>", desc = "Refresh PR (Octo)" },
 
 		{ "<leader>a", "", desc = "+assignee (Octo)", ft = "octo" },
 		{ "<leader>c", "", desc = "+comment/code (Octo)", ft = "octo" },
