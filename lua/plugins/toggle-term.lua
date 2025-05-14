1e24190a2c5ce0652163c71e1877ac5a460e8610
alejandro-pc <Alejandropatinoc@gmail.com>
2025-05-14 13:03:32 -0700

    removed unused settings for toggle term

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -14,8 +14,6 @@ return {
 					end
 				end,
 				open_mapping = [[<c-.>]],
-				hide_numbers = true,
-				shade_filetypes = {},
 				shade_terminals = true,
 				shading_factor = 2,
 				start_in_insert = true,
