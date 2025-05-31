8eb81f16b7aa1403c537ca14e4fe478b103461f8
alejandro-pc <Alejandropatinoc@gmail.com>
2025-05-31 00:52:08 -0700

    removed unused tables in lazygit

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,15 +1,8 @@
 return {
 	"folke/snacks.nvim",
-	---@type snacks.Config
-	---@class snacks.lazygit.Config: snacks.terminal.Opts
-	---@field args? string[]
-	---@field theme? snacks.lazygit.Theme
 	opts = {
 		lazygit = {
 			configure = false,
-			theme = {
-				selectedLineBgColor = { bg = "default" }, -- set to `default` to have no background colour
-			},
 		},
 	},
 }
