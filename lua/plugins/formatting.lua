eae9e56cc5d405decf9c337a3b7e0c9b7b34dc19
alejandro-pc <Alejandropatinoc@gmail.com>
2025-08-07 23:12:34 -0700

    added conform config for prettier

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,12 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      ["html"] = { "prettier" },
+      ["javascript"] = { "prettier" },
+      ["typescript"] = { "prettier" },
+      ["css"] = { "prettier" },
+    },
+  },
+}
+
