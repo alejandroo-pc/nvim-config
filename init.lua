53df65c1ed333554fee47cc61dfe66def1e5ab66
alejandro-pc <Alejandropatinoc@gmail.com>
2025-11-24 20:33:04 -0800

    set git diff custom colorscheme

--- init.lua
+++ init.lua
@@ -39,10 +39,10 @@ require("neo-tree").setup({
 })
 
 -- -- diff colorscheme
--- vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#20303b" })
--- vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#37222c" })
--- vim.api.nvim_set_hl(0, "DiffChange", { bg = "#1f2231" })
--- vim.api.nvim_set_hl(0, "DiffText", { bg = "#394b70" })
+vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#20303b" })
+vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#37222c" })
+vim.api.nvim_set_hl(0, "DiffChange", { bg = "#1f2231" })
+vim.api.nvim_set_hl(0, "DiffText", { bg = "#394b70" })
 --
 local dap = require("dap")
 
