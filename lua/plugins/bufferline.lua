18b2051d75b11e1a6763b8946531510a4beef531
alejandro-pc <Alejandropatinoc@gmail.com>
2025-11-24 20:32:43 -0800

    added bufferline to transparent

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -9,18 +9,21 @@ return {
 				sort_by = "insert_after_current",
 				diagnostics = "nvim_lsp",
 				always_show_bufferline = true,
-				show_buffer_close_icons = false,
-        show_buffer_icons=false,
+				show_buffer_icons = false,
 				-- show_close_icon = false,
 				-- color_icons = true,
 				tab_size = 15,
-				style_preset = bufferline.style_preset.minimal,
 			},
 		})
-		vim.api.nvim_set_hl(0, "BufferLineIndicatorSelected", { underline = false })
+
+		vim.g.transparent_groups = vim.list_extend(
+			vim.g.transparent_groups or {},
+			vim.tbl_map(function(v)
+				return v.hl_group
+			end, vim.tbl_values(require("bufferline.config").highlights))
+		)
 
 		local keymap = vim.api.nvim_set_keymap
-
 		keymap("n", "==", "<cmd>BufferLineMoveNext<CR>", { noremap = true, silent = true, desc = "Move buffer right" })
 		keymap("n", "--", "<cmd>BufferLineMovePrev<CR>", { noremap = true, silent = true, desc = "Move buffer left" })
 	end,
