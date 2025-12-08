3b3aa75a546737335b094c193cef55b11516707b
alejandro-pc <Alejandropatinoc@gmail.com>
2025-12-08 15:34:26 -0800

    set bufferline preset to minimal

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -9,10 +9,12 @@ return {
 				sort_by = "insert_after_current",
 				diagnostics = "nvim_lsp",
 				always_show_bufferline = true,
+				show_buffer_close_icons = false,
 				show_buffer_icons = false,
 				-- show_close_icon = false,
 				-- color_icons = true,
 				tab_size = 15,
+				style_preset = bufferline.style_preset.minimal,
 			},
 		})
 
