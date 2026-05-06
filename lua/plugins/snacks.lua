cb2fd5a04b2d7c869700c5fa412a1e9ed731f263
alejandro-pc <alejandropatinoc@gmail.com>
2026-05-06 10:31:09 -0700

    set lazugit to configure false

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,8 +1,11 @@
 return {
 	{
 		"folke/snacks.nvim",
+		name = "github_light",
 		opts = {
 			terminal = { enabled = false }, -- Disable snacks terminal, use toggleterm instead
+			lazygit = { configure = false },
+			picker = { enabled = true },
 		},
 	},
 }
