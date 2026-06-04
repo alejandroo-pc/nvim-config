3091cd0539afd7ccbb5332bf66a92a6f54b5643a
alejandro-pc <alejandropatinoc@gmail.com>
2026-06-04 08:06:43 -0700

    fixed tag completion error requiring cmp

--- lua/plugins/tag-completion.lua
+++ lua/plugins/tag-completion.lua
@@ -7,10 +7,7 @@ return {
 		"hrsh7th/nvim-cmp",
 	},
 	config = function()
-		-- import nvim-autopairs
 		local autopairs = require("nvim-autopairs")
-
-		-- configure autopairs
 		autopairs.setup({
 			check_ts = true, -- enable treesitter
 			ts_config = {
@@ -19,13 +16,10 @@ return {
 			},
 		})
 
-		-- import nvim-autopairs completion functionality
-		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-
-		-- import nvim-cmp plugin (completions plugin)
-		local cmp = require("cmp")
-
-		-- make autopairs and completion work together
-		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		local ok, cmp = pcall(require, "cmp")
+		if ok then
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end
 	end,
 }
