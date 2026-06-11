cdcb98566bded791b4875f7aa54fbbce77754fd2
alejandro-pc <alejandropatinoc@gmail.com>
2026-06-10 23:08:36 -0700

    double esc sets normal mode in terminal

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -25,6 +25,7 @@ return {
 
 			function _G.set_terminal_keymaps()
 				local opts = { noremap = true }
+				vim.api.nvim_buf_set_keymap(0, "t", "<Esc><Esc>", [[<C-\><C-n>]], opts)
 				vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>l]], opts)
 			end
 
